dd0f5dec187e6673ccb0c94f697552d9eeee3d2f
1999onell <1999onell@gmail.com>
2021-03-19 17:41:22 +0900

    백엔드에서 로그인 기능 구현

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@
 
 //모듈
 const express = require('express');
+const bodyParser = require("body-parser");
 const app = express();
 
 //라우팅
@@ -13,6 +14,9 @@ app.set("view engine", "ejs");
 
 //정적 경로 추가 미들웨어
 app.use(express.static(`${__dirname}/src/public`));
+app.use(bodyParser.json());
+//URL을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use("/", home); // use -> 미들웨어
 
