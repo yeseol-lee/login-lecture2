5776784aebb44bf105cf001acd1789b3eecd914d
1999onell <1999onell@gmail.com>
2021-03-19 16:53:22 +0900

    프런트단 javascipt파일 만들기

--- app/app.js
+++ app/app.js
@@ -11,8 +11,11 @@ const home = require("./src/routes/home");
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
+//정적 경로 추가 미들웨어
+app.use(express.static(`${__dirname}/src/public`));
 
 app.use("/", home); // use -> 미들웨어
+
 
 
 module.exports = app;
